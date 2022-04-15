05026a9d046b0fc3dd190037a304db0d64400fe8
Juan Dario Villa Vega <dariojuan85@gmail.com>
2022-04-15 11:14:13 -0500

    Primer comit, 3 animaciones, avatar creado, texturizado y animado

---
+++ Mariposa.cpp
@@ -0,0 +1,48 @@
+#include "Mariposa.h"
+
+Mariposa::Mariposa(float x, float y, float z) {
+	posx = x;
+	posy = y;
+	posz = z;
+	mariposa_cuerpo = Model();
+	mariposa_cuerpo.LoadModel("Models/mariposa/cuerpo.obj");
+	mariposa_ala1 = Model();
+	mariposa_ala1.LoadModel("Models/mariposa/ala1.obj");
+	mariposa_ala2 = Model();
+	mariposa_ala2.LoadModel("Models/mariposa/ala2.obj");
+}
+
+void Mariposa:: aleteo() {
+	if (direccion_aleteo) {
+		rotacion_ala = rotacion_ala - 0.1f;
+		if (rotacion_ala <= -35.0f)
+			direccion_aleteo = false;
+	}
+	else {
+		rotacion_ala = rotacion_ala + 0.1f;
+		if (rotacion_ala >= -0.0f)
+			direccion_aleteo = true;
+	}
+	anguloy = anguloy + 0.1f;
+	angulo_trayecto = angulo_trayecto + 0.1f;
+	plusym = 5 * sin(anguloy*toRadians);
+	plusxm = 10 * cos(angulo_trayecto*toRadians);
+	pluszm = 5 * sin(angulo_trayecto*toRadians);
+}
+void Mariposa::dibujar(GLuint uniformModel) {
+	model = glm::mat4(1.0);
+	model = glm::translate(model, glm::vec3(posx + plusxm, posy + plusym, posz + pluszm));
+	//model = glm::rotate(model, 90.0f+angulo_trayecto*toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+	glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+	mariposa_cuerpo.RenderModel();
+
+	modelaux = glm::translate(model, glm::vec3(0.0f, 0.736f, 0.38f));
+	modelaux = glm::rotate(modelaux, -rotacion_ala * toRadians, glm::vec3(1.0f, 0.0f, 0.0f));
+	glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+	mariposa_ala1.RenderModel();
+
+	modelaux = glm::translate(model, glm::vec3(0.0f, 0.736f, -0.38f));
+	modelaux = glm::rotate(modelaux, rotacion_ala*toRadians, glm::vec3(1.0f, 0.0f, 0.0f));
+	glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+	mariposa_ala2.RenderModel();
+}
---
+++ palmera_animacion.h
@@ -0,0 +1,28 @@
+#pragma once
+#include"Model.h"
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+class palmera_animacion
+{
+public:
+	const float toRadians = 3.14159265f / 180.0f;
+
+	palmera_animacion( float x, float y, float z);
+	void animacion(bool *activar);
+	void dibujar(GLuint uniformModel);
+private:
+	Model sillon;
+	Model palmera;
+	Model wumpa_palmera;
+
+	float plusx = 0.0f, plusy = 0.0f, plusz = 0.0f, rot_wumpa = 0.0f;
+	float posx, posy, posz;
+	int edo = 0;
+	glm::mat4 model, modelaux;
+
+};
+
